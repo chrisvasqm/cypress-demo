200332d5295daee5c12a991d675dffa52d8f0793
chrisvasqm <christianvasquezmelo@gmail.com>
2022-07-08 20:32:37 -0400

    Add test

--- cypress/e2e/automationpractice.cy.js
+++ cypress/e2e/automationpractice.cy.js
@@ -5,5 +5,16 @@ describe('Automation Practice Tests', () => {
 
   it('Can load the page', () => {
     cy.visit('https://demoqa.com/elements')
+    cy.get('span').contains('Text Box').click()
+    cy.get('#userName').type('John Doe')
+    cy.get('#userEmail').type("john.doe@gmail.com")
+    cy.get('#currentAddress').type('5th street')
+    cy.get('#permanentAddress').type('4th street')
+    cy.get('#submit').click()
+
+    cy.get('p[id="name"]').should('have.text', 'Name:John Doe')
+    cy.get('p[id="email"]').should('have.text', 'Email:john.doe@gmail.com')
+    cy.get('p[id="currentAddress"]').should('have.text', 'Current Address :5th street ')
+    cy.get('p[id="permanentAddress"]').should('have.text', 'Permananet Address :4th street')
   })
 })
