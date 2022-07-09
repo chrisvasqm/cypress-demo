dc5fba79687a713411710a8eaf570811b630d5fa
chrisvasqm <christianvasquezmelo@gmail.com>
2022-07-08 20:32:21 -0400

    Prevent failure on uncaught exceptions

--- cypress/e2e/automationpractice.cy.js
+++ cypress/e2e/automationpractice.cy.js
@@ -1,4 +1,8 @@
 describe('Automation Practice Tests', () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+
   it('Can load the page', () => {
     cy.visit('https://demoqa.com/elements')
   })
